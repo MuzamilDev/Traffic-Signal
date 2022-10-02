83a32336ca3787aac9b332b53f2bacfc9cf71b6a
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2022-10-02 21:04:53 +0500

    Add files via upload

---
+++ TrafficSignal.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char dir[10];
+    int left, right;
+
+    cout<<"What's your direction  i,e East, West"<<endl;
+    cin>>dir;
+
+    cout<<"Enter number of traffic from Left: "<<endl;
+    cin>>left;
+
+    cout<<"Enter Number of traffic from Right: "<<endl;
+    cin>>right;
+
+    if(left==0 && right==0)
+        cout<<"GREEN LIGHT "<<endl;
+    if(left<=9 && right<=9)
+        cout<<"Ready For Go "<<endl;
+    if(left>9 && right>9)
+        cout<<"RED LIGHT "<<endl;
+
+        return 0;
+}
